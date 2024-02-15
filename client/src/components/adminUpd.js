cb7b497a7976a8215450f70509090b577379508e
shrncr <loveyawink@gmail.com>
2024-02-15 09:22:35 -0600

    added comments to source code

--- client/src/components/adminUpd.js
+++ client/src/components/adminUpd.js
@@ -1,27 +1,28 @@
 import React, { useState } from 'react';
 import axios from 'axios'; 
-
+//change the ui
 function ExhibitAdd() {
-  const [name, setName] = useState('');
+  //name and descriptions are variables which start as emty strings. upone calling setname/setdesc, they will change to whatever is defined
+  const [name, setName] = useState(''); 
   const [description, setDescription] = useState('');
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post('http://localhost:5000', { name, description });
+      await axios.post('http://localhost:5000', { name, description }); //post name and description as json obj to base route of the server
       // Clear form after successful submission
       setName('');
       console.log("gibrd");
       setDescription('');
-      alert('exhibit added');
-    } catch (error) {
+      alert('exhibit added'); 
+    } catch (error) { //error handling
       console.error('error:', error);
       alert('An error occured.');
     }
   };
-
+  //the html returned
   return (
-    <div>
+    <div> 
       <h2>Add Exhibit</h2>
       <form onSubmit={handleSubmit}>
         <div>
