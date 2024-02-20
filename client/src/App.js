e9877a1cf60936c961ac0d15a1fcc1ca274260b5
shrncr <loveyawink@gmail.com>
2024-02-20 10:19:12 -0600

    added dif paths

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
 import logo from './logo.svg';
+import React, {Component} from "react";
 import './App.css';
+import reactDOM from "react-dom";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ExhibitAdd from './components/adminUpd'
 
 function App() {
+
   return (
-    <ExhibitAdd/>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<ExhibitAdd/>} />
+      <Route path="/map" element={<ExhibitAdd/>} />
+      <Route path="/playstyles" element={<ExhibitAdd/>} />
+    </Routes>
+  </BrowserRouter>
   );
 }
 
