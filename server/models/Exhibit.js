c36d788b14416c085eae4d0422ff97e097f26f61
zac swisher <zhswisher@gmail.com>
2024-02-21 19:52:11 -0500

    exhibit schema completed

--- server/models/Exhibit.js
+++ server/models/Exhibit.js
@@ -1,16 +1,25 @@
 const mongoose = require("mongoose");
 const { ObjectId } = require('mongodb');
 const ExhibitSchema = new mongoose.Schema({ //schema for the exhibits. more to be added soon
-    ID:{
+    exhibit_id:{
         type:ObjectId,
-        required:true
+        required:true,
     },
-    title: {
+    title:{
         type:String,
         required:true,
     },
     desc:{
         type:String,
+        required:true,
+    },
+    photo:{
+        type:String,
+        required:true, // string will be the URL pointing to the image stored externally
+    },
+    // is the exhibit active or hidden
+    status:{
+        type:Boolean,
         required:true,
     },
 })
