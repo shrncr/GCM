681f4ed57e9e285344dffd495d618257175db666
zac swisher <zhswisher@gmail.com>
2024-03-11 18:36:25 -0400

    insert script for map

---
+++ server/insertScripts/insertMap.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const Map = require('../models/Map');
+
+// mongodb connection string
+const mongoURI = 'mongodb+srv://swish:xCjo8H8cbamHr5CQ@gcm.odjguc7.mongodb.net/';
+
+// sample data
+const MapData = [
+    {
+        map_id: new mongoose.Types.ObjectId(),
+        longitude: -82.50261707677507,
+        latitude: 27.901313674533228,
+        desc: "The YMCA is a place for kids to be active."
+    },
+    {
+        map_id: new mongoose.Types.ObjectId(),
+        longitude: -82.48147005267664,
+        latitude: 27.88930362717901,
+        desc: "Ballast point park."
+    }
+];
+
+// connect to MongoDB w/ error messages
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.error('Could not connect to MongoDB', err));
+
+// function to insert sample data
+const insertSampleData = async () => {
+    try {
+        await Map.insertMany(MapData);
+        console.log('Sample data inserted successfully');
+    } catch (error) {
+        console.error('Error inserting sample data', error);
+    } finally {
+        mongoose.connection.close();
+    }
+};
+
+insertSampleData();
