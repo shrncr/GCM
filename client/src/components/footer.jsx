a851b456b01ef47d2642f3e1dd80ff03b48aefbb
zac swisher <zhswisher@gmail.com>
2024-04-11 17:33:56 -0400

    new footer indentical to https://glazermuseum.org/

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -4,15 +4,27 @@ import React from 'react';
 function Footer() {
     return (
       <footer className="footer">
-        This is the footer. You can add your text here.
         <div className="footer-content">
-        <p>This is the footer. You can add your text here.</p>
-        <p>This is the footer. You can add your text here.</p>
-        <p>This is the footer. You can add your text here.</p>
+                <div style={{fontWeight: 'bold' }}>
+                    <a href="https://glazermuseum.org/visit">Plan Your Visit</a>
+                    {" | "}
+                    <a href="https://glazermuseum.org/contact">Contact</a>
+                    {" | "}
+                    <a href="https://glazermuseum.org/news">News</a>
+                    {" | "}
+                    <a href="https://glazermuseum.org/contact">Contact</a>
+                    {" | "}
+                    <a href="https://glazermuseum.org/visit/#hours">Hours of Operation</a>
+                </div>
 
-        <p>This is the footer. You can add your text here.</p>
-      </div>
-      </footer>
+                <div style={{fontWeight: '100' }}>
+                    Glazer Children’s Museum | 110 W Gasparilla Plaza, Tampa FL 33602 
+                    <a href="tel:+18134433861"> | 813 443 3861</a>
+                    <br />
+                    Copyright © 2022 | All rights reserved | Tax ID 59-2637851 | Registration #CH20272
+                </div>
+                </div>
+        </footer>
     );
   }
   
