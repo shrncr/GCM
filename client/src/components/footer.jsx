acae49443873e7cb78c7d5fa9683d591ae3f11b1
zac swisher <zhswisher@gmail.com>
2024-04-11 18:33:57 -0400

    added about and fixed font weight

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -5,25 +5,28 @@ function Footer() {
     return (
       <footer className="footer">
         <div className="footer-content">
-                <div style={{fontWeight: 'bold' }}>
-                    <a href="https://glazermuseum.org/visit">Plan Your Visit</a>
+        <div>
+                    <a href="/about" style={{fontWeight: 'bold' }}>About</a>
                     {" | "}
-                    <a href="https://glazermuseum.org/contact">Contact</a>
+                    <a href="https://glazermuseum.org/visit" style={{fontWeight: 'bold' }}>Plan Your Visit</a>
                     {" | "}
-                    <a href="https://glazermuseum.org/news">News</a>
+                    <a href="https://glazermuseum.org/contact" style={{fontWeight: 'bold' }}>Contact</a>
                     {" | "}
-                    <a href="https://glazermuseum.org/contact">Contact</a>
+                    <a href="https://glazermuseum.org/news" style={{fontWeight: 'bold' }}>News</a>
                     {" | "}
-                    <a href="https://glazermuseum.org/visit/#hours">Hours of Operation</a>
+                    <a href="https://glazermuseum.org/contact" style={{fontWeight: 'bold' }}>Contact</a>
+                    {" | "}
+                    <a href="https://glazermuseum.org/visit/#hours" style={{fontWeight: 'bold' }}>Hours of Operation</a>
                 </div>
 
                 <div style={{fontWeight: '100' }}>
                     Glazer Children’s Museum | 110 W Gasparilla Plaza, Tampa FL 33602 
-                    <a href="tel:+18134433861"> | 813 443 3861</a>
+                    <a href="tel:+18134433861"> | 813-443-3861</a>
                     <br />
                     Copyright © 2022 | All rights reserved | Tax ID 59-2637851 | Registration #CH20272
                 </div>
-                </div>
+
+          </div>
         </footer>
     );
   }
