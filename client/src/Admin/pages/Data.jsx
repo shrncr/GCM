e3d45d65c5ef6dbfab9240757d53a53e82e08019
MurawskiC1 <carter.murawski@spartans.ut.edu>
2024-04-18 11:28:37 -0400

    Data page

--- client/src/Admin/pages/Data.jsx
+++ client/src/Admin/pages/Data.jsx
@@ -3,50 +3,61 @@ import { ExhibitContext } from "../SetData"
 import { Link } from "react-router-dom";
 import { CSVLink } from "react-csv"
 import axios from 'axios';
- 
+
 export default function Data(props) {
     const { data } = useContext(ExhibitContext);
     const [spread, setSpread] = useState(data[0]);
+    let loc = "Feedback"
     let index = props.index
-    const handleSpreadChange = (newindex) => {
+    const handleSpreadChange = (newindex, l) => {
+        loc = l
         index = newindex
         setSpread(data[newindex]);
     };
 
     // button to download impression data
     const handleDownload = async () => {
-        case1 = 'impressions';
-            try {
-                const response = await axios.get('http://localhost:8082/download-impressions-csv', {
-                    responseType: 'blob',
-                });
-                const url = window.URL.createObjectURL(new Blob([response.data]));
-                const link = document.createElement('a');
-                link.href = url;
-                link.setAttribute('download', 'impressions.csv');
-                document.body.appendChild(link);
-                link.click();
-                link.parentNode.removeChild(link);
-            } catch (error) {
-                console.error('Error downloading the CSV:', error);
-            }
-        };
+        switch (loc) {
+            case "Feedback":
+                break;
+            case "Impressions":
+                try {
+                    const response = await axios.get('http://localhost:8082/download-impressions-csv', {
+                        responseType: 'blob',
+                    });
+                    const url = window.URL.createObjectURL(new Blob([response.data]));
+                    const link = document.createElement('a');
+                    link.href = url;
+                    link.setAttribute('download', 'impressions.csv');
+                    document.body.appendChild(link);
+                    link.click();
+                    link.parentNode.removeChild(link);
+                } catch (error) {
+                    console.error('Error downloading the CSV:', error);
+                }
+                break;
+
+            case "Session":
+                break;
+        }
+    };
 
 
 
     return (
         <>
-            <h1 className='header'>Data</h1>
-
+            <div>
+                <h1 className='header'>Data</h1>
+            </div>
             <div className="dropdown">
 
                 <div className='dropdown-button'>
                     Select Data
                 </div>
                 <div className="dropdown-menu">
-                    <button onClick={() => handleSpreadChange(0)}>Type2</button>
-                    <button onClick={() => handleSpreadChange(1)}>Type2</button>
-                    <button onClick={() => handleSpreadChange(2)}>Type3</button>
+                    <button onClick={() => handleSpreadChange(0, "Feedback")}>Feedback</button>
+                    <button onClick={() => handleSpreadChange(1, "Impressions")}>Impressions</button>
+                    <button onClick={() => handleSpreadChange(2, "Session")}>Session</button>
                 </div>
             </div>
 
@@ -73,7 +84,7 @@ export default function Data(props) {
 
             </div>
 
-            <div className="download"> 
+            <div className="download">
                 <button onClick={handleDownload}>Download</button>
             </div>
         </>
