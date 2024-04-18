b1f0af23754e359ca48dbe018754521a71bcff96
ltalham <123185079+ltalham@users.noreply.github.com>
2024-04-18 14:58:28 -0400

    made some changes to the accoridon in the single info component

--- client/src/components/mapx.jsx
+++ client/src/components/mapx.jsx
@@ -8,73 +8,103 @@
 
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
+import { GoogleMap, useLoadScript, Marker, InfoWindow } from '@react-google-maps/api';
+
 const libraries = ['places'];
 const mapContainerStyle = {
-  width: '100vw',
+  width: '70vw', // Adjusted width for the map
   height: '100vh',
 };
-const center = { //where center of map is (tampa)
+
+const listContainerStyle = {
+  width: '30vw', // Width for the list container
+  height: '100vh',
+  overflowY: 'auto', // Enable vertical scrolling
+};
+
+const center = {
   lat: 27.9469,
   lng: -82.4672,
 };
 
 function Map(props) {
-  const markerContent = props.markerContent;
   const [locations, setLocations] = useState([]);
+  const [selectedLocation, setSelectedLocation] = useState(null);
 
-  useEffect(() => { //get pins based on learning styles defined (whether it be all or specific one)
-    axios({
-      url: 'http://localhost:8082/map',
-      method: 'GET',
+  useEffect(() => {
+    axios.get('http://localhost:8082/map', {
       params: {
-        filter: markerContent
+        filter: props.markerContent
       },
       headers: {
         authorization: 'mongodb+srv://sarahrnciar:m66Wpq4mggMTOZw8@admin.eqktqv7.mongodb.net/?retryWrites=true&w=majority',
-      },
-      catch(error) {
-        console.error('error:', error);
-        alert('An error occured.')
       }
-    }).then((res) => {
-      setLocations(res.data)
+    }).then(res => {
+      setLocations(res.data);
+    }).catch(error => {
+      console.error('Error:', error);
+      alert('An error occurred.');
     });
-  }, [locations]);
+  }, [props.markerContent]);
 
-
-
-  const { isLoaded, loadError } = useLoadScript({ //loading map
+  const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: "AIzaSyBXqEo_870tbuzkTgjEondNHYznmrEnVf8",
     libraries,
   });
+
+  const handleMarkerClick = (location) => {
+    setSelectedLocation(location);
+  };
 
   if (loadError) {
     return <div>Error loading maps</div>;
   }
 
-  if (!isLoaded) { //say loading til loaded
+  if (!isLoaded) {
     return <div>Loading maps</div>;
   }
 
   return (
-    <div>
-      <GoogleMap //return a map
+    <div className="map-container">
+      <GoogleMap
         mapContainerStyle={mapContainerStyle}
         zoom={10}
         center={center}
       >
-        {locations.map(curpin => ( //put pins on the map
-          <Marker position={
-            {
-              lat: curpin.latitude,
-              lng: curpin.longitude
-            }}
+        {locations.map(location => (
+          <Marker
+            key={location._id}
+            position={{ lat: location.latitude, lng: location.longitude }}
+            onClick={() => handleMarkerClick(location)}
           />
         ))}
+        {selectedLocation && (
+          <InfoWindow
+            position={{ lat: selectedLocation.latitude, lng: selectedLocation.longitude }}
+            onCloseClick={() => setSelectedLocation(null)}
+          >
+            <div>
+              <h2>{selectedLocation.name}</h2>
+              <p>Latitude: {selectedLocation.latitude}</p>
+              <p>Longitude: {selectedLocation.longitude}</p>
+            </div>
+          </InfoWindow>
+        )}
       </GoogleMap>
+      <div className="list-container">
+        <h2>List of Places</h2>
+        <ul>
+          {locations.map(location => (
+            <li key={location._id} onClick={() => handleMarkerClick(location)}>
+              {location.name}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
-};
+}
 
 export default Map;
+
+
--- client/src/components/playInfo.jsx
+++ client/src/components/playInfo.jsx
@@ -1,46 +1,61 @@
-//displays the play info page
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Banner from './banner';
+import axios from 'axios';
 import Accordion from './accordion';
-import playExample from '../components/images/playExample.webp'; // Corrected import path
+import playExample from '../components/images/playExample.webp';
 import Footer from './footer';
 
-function PlayInfo() {
+function PlayInfo({ title }) {
+    const [desc, setDesc] = useState([]);
+    const [skills, setSkills] = useState([]);
+
+    useEffect(() => {
+        // Fetch data based on the title
+        axios.get(`http://localhost:8082/${title}`)
+            .then((res) => {
+                setDesc(res.data.baseData.desc);
+                setSkills(res.data.dropdown);
+            })
+            .catch((err) => {
+                console.log('Error fetching data: ', err);
+            });
+    }, [title]);
+
     const data = [
-        { 
+        {
             id: 0,
-            label: "About Play style 1",
+            label: `About ${title}`,
             renderContent: () => (
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, harum natus! Voluptatibus libero ratione vitae repellat, quasi vero tempora recusandae beatae deserunt sint voluptate doloremque maxime eveniet soluta similique dignissimos.
-            </p>
+                <p>
+                    {desc}
+                </p>
             ),
         },
         {
             id: 1,
             label: "Skills Taught",
             renderContent: () => (
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, harum natus! Voluptatibus libero ratione vitae repellat, quasi vero tempora recusandae beatae deserunt sint voluptate doloremque maxime eveniet soluta similique dignissimos.
-            </p>
+                <p>
+                    {skills}
+                </p>
             ),
         },
         {
             id: 2,
             label: "Ways to Play",
             renderContent: () => (
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, harum natus! Voluptatibus libero ratione vitae repellat, quasi vero tempora recusandae beatae deserunt sint voluptate doloremque maxime eveniet soluta similique dignissimos.
-            </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, harum natus! Voluptatibus libero ratione vitae repellat, quasi vero tempora recusandae beatae deserunt sint voluptate doloremque maxime eveniet soluta similique dignissimos.
+                </p>
             ),
         },
         {
             id: 3,
             label: "More Resources",
             renderContent: () => (
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, harum natus! Voluptatibus libero ratione vitae repellat, quasi vero tempora recusandae beatae deserunt sint voluptate doloremque maxime eveniet soluta similique dignissimos.
-            </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, harum natus! Voluptatibus libero ratione vitae repellat, quasi vero tempora recusandae beatae deserunt sint voluptate doloremque maxime eveniet soluta similique dignissimos.
+                </p>
             ),
         }
     ];
@@ -48,14 +63,15 @@ function PlayInfo() {
     return (
         <div>
             <Banner className="home-background" text="Welcome" />
-            <h2>About Play Style 1</h2>
+            <h2>About {title}</h2>
             <hr />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi nihil doloremque atque deserunt molestiae vel cupiditate necessitatibus velit sequi magnam, dolore consequatur suscipit ad enim nesciunt tenetur voluptate repellendus repellat.</p>
+
             <div className="container">
                 <img src={playExample} alt="Logo" className='PlayInfo-img' />
-                <Accordion items={data} keepOthersOpen={true}/>
-            
+                <Accordion items={data} keepOthersOpen={true} />
             </div>
-        <Footer />
+            <Footer />
         </div>
     );
 }
