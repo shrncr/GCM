ba9e7cbc0f2253851d93e8c326ac3bf91571e490
ltalham <123185079+ltalham@users.noreply.github.com>
2024-04-18 18:22:40 -0400

    edited map.jsx

--- client/src/components/mapx.jsx
+++ client/src/components/mapx.jsx
@@ -1,110 +1,72 @@
-/*
-* Map component
-* Google map which loads in pins of playlocations whose "visible" property is set to true
-* Props takes in editing=true or false. False when on the museum page, true when admin page
-* Props take in MarkerContent. What types of playstyles?
-* edit=true allows dunti
-*/
-
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { GoogleMap, useLoadScript, Marker, InfoWindow } from '@react-google-maps/api';
-
+import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
 const libraries = ['places'];
 const mapContainerStyle = {
-  width: '70vw', // Adjusted width for the map
+  width: '100vw',
   height: '100vh',
 };
-
-const listContainerStyle = {
-  width: '30vw', // Width for the list container
-  height: '100vh',
-  overflowY: 'auto', // Enable vertical scrolling
-};
-
-const center = {
+const center = { //where center of map is (tampa)
   lat: 27.9469,
   lng: -82.4672,
 };
 
 function Map(props) {
+  const markerContent = props.markerContent;
   const [locations, setLocations] = useState([]);
-  const [selectedLocation, setSelectedLocation] = useState(null);
 
-  useEffect(() => {
-    axios.get('http://localhost:8082/map', {
+  useEffect(() => { //get pins based on learning styles defined (whether it be all or specific one)
+    axios({
+      url: 'http://localhost:8082/map',
+      method: 'GET',
       params: {
-        filter: props.markerContent
+        filter: markerContent
       },
       headers: {
         authorization: 'mongodb+srv://sarahrnciar:m66Wpq4mggMTOZw8@admin.eqktqv7.mongodb.net/?retryWrites=true&w=majority',
+      },
+      catch(error) {
+        console.error('error:', error);
+        alert('An error occured.')
       }
-    }).then(res => {
-      setLocations(res.data);
-    }).catch(error => {
-      console.error('Error:', error);
-      alert('An error occurred.');
+    }).then((res) => {
+      setLocations(res.data)
     });
-  }, [props.markerContent]);
+  }, [locations]);
 
-  const { isLoaded, loadError } = useLoadScript({
+
+
+  const { isLoaded, loadError } = useLoadScript({ //loading map
     googleMapsApiKey: "AIzaSyBXqEo_870tbuzkTgjEondNHYznmrEnVf8",
     libraries,
   });
-
-  const handleMarkerClick = (location) => {
-    setSelectedLocation(location);
-  };
 
   if (loadError) {
     return <div>Error loading maps</div>;
   }
 
-  if (!isLoaded) {
+  if (!isLoaded) { //say loading til loaded
     return <div>Loading maps</div>;
   }
 
   return (
-    <div className="map-container">
-      <GoogleMap
-        mapContainerStyle={mapContainerStyle}
-        zoom={10}
-        center={center}
-      >
-        {locations.map(location => (
-          <Marker
-            key={location._id}
-            position={{ lat: location.latitude, lng: location.longitude }}
-            onClick={() => handleMarkerClick(location)}
-          />
-        ))}
-        {selectedLocation && (
-          <InfoWindow
-            position={{ lat: selectedLocation.latitude, lng: selectedLocation.longitude }}
-            onCloseClick={() => setSelectedLocation(null)}
-          >
-            <div>
-              <h2>{selectedLocation.name}</h2>
-              <p>Latitude: {selectedLocation.latitude}</p>
-              <p>Longitude: {selectedLocation.longitude}</p>
-            </div>
-          </InfoWindow>
-        )}
-      </GoogleMap>
-      <div className="list-container">
-        <h2>List of Places</h2>
-        <ul>
-          {locations.map(location => (
-            <li key={location._id} onClick={() => handleMarkerClick(location)}>
-              {location.name}
-            </li>
+      <div>
+        <GoogleMap //return a map
+            mapContainerStyle={mapContainerStyle}
+            zoom={10}
+            center={center}
+        >
+          {locations.map(curpin => ( //put pins on the map
+              <Marker position={
+                {
+                  lat: curpin.latitude,
+                  lng: curpin.longitude
+                }}
+              />
           ))}
-        </ul>
+        </GoogleMap>
       </div>
-    </div>
   );
-}
+};
 
 export default Map;
-
-
