4d3d4fcd3b4000572daa3e0815f137e5ef000b73
zac swisher <zhswisher@gmail.com>
2024-04-28 23:17:02 -0400

    allow all sites

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const app = express();
 
 // Add Access Control Allow Origin headers
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://gcm-frontend.vercel.app");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
