15e4497ca7b08b05848776ed4ac0fea385931fd6
shrncr <loveyawink@gmail.com>
2024-04-29 18:49:07 -0500

    no feedback id in schema

--- server/models/Feedback.js
+++ server/models/Feedback.js
@@ -4,10 +4,6 @@ Feedback schema on a particular exhibit
 const mongoose = require("mongoose");
 const { ObjectId } = require('mongodb');
 const FeedbackSchema = new mongoose.Schema({ 
-    feedback_id:{
-        type:ObjectId,
-        required:true,
-    },
     exhibitId:{
         type:String,
         required:true, // string will be the URL pointing to the image stored externally
