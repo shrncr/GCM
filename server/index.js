e79b9374c81832b37b200e20e3f0209fad3df746
shrncr <loveyawink@gmail.com>
2024-05-20 15:28:40 -0500

    back to not local

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const MongoStore = require('connect-mongo');
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:3000', // allow requests from frontend
+  origin: 'https://gcm-frontend.vercel.app', // allow requests from frontend
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed  methods
   allowedHeaders: ['Content-Type', 'Authorization','User-Agent', 'Accept', 'Referer'], // headers to allow
   credentials: true, // allow setting of cookies or sessions
