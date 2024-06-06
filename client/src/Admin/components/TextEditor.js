c2d0e5902b5bb35e3a280ec56581185520d264a1
shrncr <loveyawink@gmail.com>
2024-06-06 09:36:36 -0500

    sizes

--- client/src/Admin/components/TextEditor.js
+++ client/src/Admin/components/TextEditor.js
@@ -2,28 +2,32 @@ import React from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
+
+const Font = ReactQuill.Quill.import('formats/font');
+ // Set the whitelist for the fonts you want to include
+ReactQuill.Quill.register(Font, true);
+
 const TextEditor = ({ value, onChange }) => {
     const modules = {
         toolbar: [
             ['bold', 'italic', 'underline', { 'header': 1 }, { 'header': 2 }],
             [{ 'font': [] }],
+            [{ 'size': ['small', false, 'large', 'huge'] }],
             [{ 'color': [] }, { 'background': [] }],
             [{ 'list': 'ordered'}, { 'list': 'bullet' }],
             [{ 'indent': '-1'}, { 'indent': '+1' }],
-            [{ 'size': ['small', false, 'large'] }],
             [{ 'align': [] }],
         ],
     };
-    
+
     const formats = [
         'bold', 'italic', 'underline', 'header',
-        'font',
+        'font', // Ensure 'font' is included here
+        'size',
         'color', 'background',
         'list', 'bullet', 'indent',
-        'size',
         'align'
     ];
-    
 
     const handleChange = (content, delta, source, editor) => {
         onChange(editor.getHTML()); // Use the onChange prop to update the parent's state
